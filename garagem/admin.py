f5117954a8a0a114073253653f858c274661e69f
askzxcp <102809983+Rian-2INFO3@users.noreply.github.com>
2023-06-23 10:33:35 -0300

    Update admin.py

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Mss
+
+admin.site.register(Mss)
+
 
 from django.contrib import admin
 
